1611503b55aa545d11dbc914375f4170fa8eff13
campallison <allisonhanna@gmail.com>
2016-07-25 19:59:44 -0700

    working on getting the makeAccount to render

--- components/App.js
+++ components/App.js
@@ -1,13 +1,14 @@
 import { } from 'react-bootstrap';
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import Data from './data';
+// import Data from './data';
+import makeAccount from './makeAccount';
 
 export default class App extends Component {
   render() {
     return (
-      <div id="App">
-        <p>Hello, Yandri!</p>
+      <div>
+        <makeAccount />
       </div>
     );
   }
---
+++ components/makeAccount.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+export default class makeAccount extends Component {
+  render() {
+    return(
+      <div>
+        <h1>
+          Hello, Colin!
+        </h1>
+      </div>
+    )
+  }
+}
