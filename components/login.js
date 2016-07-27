3f5a2edd00e8f72f42e0da062082e8cdadcad5dc
Yandri Sanchez <ysanchez@Yandris-MacBook-Pro.local>
2016-07-26 22:53:06 -0700

    switch between pages

--- components/login.js
+++ components/login.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 export default class Login extends Component {
   render() {
+    console.log(this.props.makeAcc)
     return (
       <div className="container">
         <label><b>Username</b></label>
@@ -11,6 +12,7 @@ export default class Login extends Component {
         <input type="password" placeholder="Enter Password" name="psw" required />
 
         <button type="submit">Login</button>
+        <button onClick={this.props.makeAcc}>Create Account</button>
       </div>
     );
   }
